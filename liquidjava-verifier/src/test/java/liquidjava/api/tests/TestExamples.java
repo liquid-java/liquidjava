ea9d845988d20561047b092cafceceb1e0cd939a
Ricardo Costa <rcosta.ms358@gmail.com>
2025-11-22 19:19:17 +0000

    Validate Ghost Invocations Before Sending to SMT
    - Validate ghost invocations instead of relying on Z3 output
    - Throw LJErrors in AST expressions

--- liquidjava-verifier/src/test/java/liquidjava/api/tests/TestExamples.java
+++ liquidjava-verifier/src/test/java/liquidjava/api/tests/TestExamples.java
@@ -56,6 +56,10 @@ public class TestExamples {
                     fail();
                 }
                 LJError error = diagnostics.getErrors().iterator().next();
+                if (error.getPosition() == null) {
+                    System.out.println("Error in: " + pathName + " --- error has no position information");
+                    fail();
+                }
                 if (expected.isPresent()) {
                     String expectedError = expected.get();
                     String foundError = error.getTitle();
