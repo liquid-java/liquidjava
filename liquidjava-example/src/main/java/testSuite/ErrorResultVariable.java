9d7e2dc138c9f335b5f9adfd7dd7179b2a392455
RajShivu <rajshivu1112@email@gmail.com>
2026-02-10 22:44:24 +0530

    feat: implement  support and restore test infrastructure

---
+++ liquidjava-example/src/main/java/testSuite/ErrorResultVariable.java
@@ -0,0 +1,10 @@
+// Not Found Error
+package testSuite;
+import liquidjava.specification.Refinement;
+
+public class ErrorResultVariable {
+    public void test() {
+        @Refinement("$result > 0") 
+        int x = 10; 
+    }
+}
