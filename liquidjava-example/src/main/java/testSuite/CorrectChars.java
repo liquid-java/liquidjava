2ac2ae0228566051160c6a77ad20b40ec0f91b36
Ricardo Costa <rcosta.ms358@gmail.com>
2026-02-23 13:38:57 +0000

    Add Test

--- liquidjava-example/src/main/java/testSuite/CorrectChars.java
+++ liquidjava-example/src/main/java/testSuite/CorrectChars.java
@@ -2,6 +2,7 @@ package testSuite;
 
 import liquidjava.specification.Refinement;
 
+@SuppressWarnings("unused")
 public class CorrectChars {
 
     @Refinement("_ == 65")
@@ -18,5 +19,18 @@ public class CorrectChars {
 
     void printLetter(@Refinement("_ >= 65 && _ <= 90 || _ >= 97 && _ <= 122") char c) {
         System.out.println(c);
+    }
+
+    void test2() {
+        @Refinement("_ == 97")
+        char c = 'a';
+        
+        @Refinement("_ == 98")
+        int res = inc(c);
+    }
+
+    @Refinement("_ == x + 1")
+    int inc(int x) {
+        return x + 1;
     }
 }
