2b771b2515db517be56ca0bb5c08c8e3cf271a18
Ricardo Costa <rcosta.ms358@gmail.com>
2026-03-21 18:46:05 +0000

    Minor Change

--- liquidjava-verifier/src/main/java/liquidjava/utils/VariableFormatter.java
+++ liquidjava-verifier/src/main/java/liquidjava/utils/VariableFormatter.java
@@ -48,6 +48,6 @@ public final class VariableFormatter {
     }
 
     private static boolean isSpecialIdentifier(String id) {
-        return "fresh".equals(id) || "ret".equals(id);
+        return id.equals("fresh") || id.equals("ret");
     }
 }
