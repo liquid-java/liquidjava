9cacea7dd1a40dad6e85cb37987ec2e6c2e3657f
Catarina Gamboa <catarina.vg@hotmail.com>
2026-05-04 16:41:41 +0100

    forgot to save

--- liquidjava-example/src/main/java/testSuite/classes/missing_import_final_error/ClassImporting.java
+++ liquidjava-example/src/main/java/testSuite/classes/missing_import_final_error/ClassImporting.java
@@ -1,9 +1,9 @@
-package testSuite.classes.error_missing_import;
+package testSuite.classes.missing_import_final_error;
 
 import javax.imageio.ImageWriteParam;
 
 /** Sibling file that imports the class so the verifier knows where to find it. */
-public class Helper {
+public class ClassImporting {
     public static int explicit() {
         return ImageWriteParam.MODE_EXPLICIT;
     }
