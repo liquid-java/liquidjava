e742b1d815d19cc6b62dd75167fb4f4328e9c142
Ricardo Costa <rcosta.ms358@gmail.com>
2026-05-18 17:52:37 +0100

    Store Derivation Nodes in State Refinement Errors

--- liquidjava-verifier/src/main/java/liquidjava/diagnostics/errors/StateRefinementError.java
+++ liquidjava-verifier/src/main/java/liquidjava/diagnostics/errors/StateRefinementError.java
@@ -1,7 +1,7 @@
 package liquidjava.diagnostics.errors;
 
 import liquidjava.diagnostics.TranslationTable;
-import liquidjava.rj_language.ast.Expression;
+import liquidjava.rj_language.opt.derivation_node.ValDerivationNode;
 import spoon.reflect.cu.SourcePosition;
 
 /**
@@ -11,23 +11,23 @@ import spoon.reflect.cu.SourcePosition;
  */
 public class StateRefinementError extends LJError {
 
-    private final String expected;
-    private final String found;
+    private final ValDerivationNode expected;
+    private final ValDerivationNode found;
 
-    public StateRefinementError(SourcePosition position, Expression expected, Expression found,
+    public StateRefinementError(SourcePosition position, ValDerivationNode expected, ValDerivationNode found,
             TranslationTable translationTable, String customMessage) {
-        super("State Refinement Error",
-                String.format("Expected state %s but found %s", expected.toDisplayString(), found.toDisplayString()),
-                position, translationTable, customMessage);
-        this.expected = expected.toDisplayString();
-        this.found = found.toDisplayString();
+        super("State Refinement Error", String.format("Expected state %s but found %s",
+                expected.getValue().toDisplayString(), found.getValue().toDisplayString()), position, translationTable,
+                customMessage);
+        this.expected = expected;
+        this.found = found;
     }
 
-    public String getExpected() {
+    public ValDerivationNode getExpected() {
         return expected;
     }
 
-    public String getFound() {
+    public ValDerivationNode getFound() {
         return found;
     }
 }
