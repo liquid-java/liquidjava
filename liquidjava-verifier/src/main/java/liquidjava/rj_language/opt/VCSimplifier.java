3fc150d9b7ca7b6030b15fbf957c53951747980a
Ricardo Costa <rcosta.ms358@gmail.com>
2026-06-08 18:34:59 +0100

    Add `simplifyOnce`

--- liquidjava-verifier/src/main/java/liquidjava/rj_language/opt/VCSimplifier.java
+++ liquidjava-verifier/src/main/java/liquidjava/rj_language/opt/VCSimplifier.java
@@ -11,4 +11,12 @@ public class VCSimplifier {
         // TODO: implement remaining simplification steps
         return VCSubstitution.apply(implication);
     }
+
+    /**
+     * Applies one simplification step to a VC chain
+     */
+    public static VCImplication simplifyOnce(VCImplication implication) {
+        // TODO: implement remaining simplification steps
+        return VCSubstitution.applyOnce(implication);
+    }
 }
