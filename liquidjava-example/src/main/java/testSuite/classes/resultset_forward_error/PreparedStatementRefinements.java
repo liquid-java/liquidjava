4be3bb1f2637c4851477adfba3dcb98407728dc2
Ricardo Costa <rcosta.ms358@gmail.com>
2026-08-11 10:43:34 +0100

    Remove Unused Imports (#286)

--- liquidjava-example/src/main/java/testSuite/classes/resultset_forward_error/PreparedStatementRefinements.java
+++ liquidjava-example/src/main/java/testSuite/classes/resultset_forward_error/PreparedStatementRefinements.java
@@ -1,13 +1,10 @@
 package testSuite.classes.resultset_forward_error;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import liquidjava.specification.ExternalRefinementsFor;
 import liquidjava.specification.Ghost;
 import liquidjava.specification.Refinement;
-import liquidjava.specification.StateRefinement;
-import liquidjava.specification.StateSet;
 
 
 @Ghost("boolean setBackwards")
